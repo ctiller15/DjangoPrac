c7e01919d6bf259b7c17ac195e262b90b3c4f0eb
ctiller15 <christiller2015@gmail.com>
2017-09-17 00:46:51 -0400

    Add views to polls.

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+	# this leads to /polls/
 	url(r'^$', views.index, name='index'),
+	# this leads to polls/(int)/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# this leads to the polls results page
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# this leads to the polls vote page
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
