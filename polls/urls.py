fdcde7c74f37c298990d1fb0aac27f8276a881eb
ctiller15 <christiller2015@gmail.com>
2017-09-17 13:38:24 -0400

    refactor into generic views, add form, update results

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 	# this leads to /polls/
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# this leads to polls/(int)/
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	# this leads to the polls results page
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	# this leads to the polls vote page
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
